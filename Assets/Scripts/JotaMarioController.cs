118be3b89271e9050d157095da6d4ab901909dfc
Andres Vargas <archarry>
2022-06-11 20:18:28 -0500

    Rotation and Moving mechanic.

--- Assets/Scripts/JotaMarioController.cs
+++ Assets/Scripts/JotaMarioController.cs
@@ -7,26 +7,23 @@ public class JotaMarioController : MonoBehaviour
     public float movementSpeed;
     private Rigidbody rb;
     // Start is called before the first frame update
-    void Start()
-    {
-     rb = gameObject.GetComponent<Rigidbody>();   
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         MovePlayer();
     }
-
-    void MovePlayer(){
+    void MovePlayer() {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * movementSpeed;
-        transform.Translate(movementDirection);
-        rb.rotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
+        if(movementDirection != Vector3.zero) {
+            var vector2 = new Vector2(-horizontalInput, verticalInput);
+            var angle = Vector2.SignedAngle(Vector2.up, vector2);
+            Debug.Log(angle + " " + vector2);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+            transform.Translate(( ( transform.forward * verticalInput ) + ( transform.right * -horizontalInput ) ) * Time.deltaTime * movementSpeed);
+        }
     }
-
-    
 }
