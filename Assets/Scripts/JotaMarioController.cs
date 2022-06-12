f5fa287d96805e7ce498363d19e85eca373f5494
Andres Vargas <archarry>
2022-06-11 21:18:48 -0500

    When there is no direction pressed the main character keeps its rotation.

--- Assets/Scripts/JotaMarioController.cs
+++ Assets/Scripts/JotaMarioController.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JotaMarioController : MonoBehaviour
-{
+public class JotaMarioController : MonoBehaviour{
+
     public float movementSpeed;
     private Rigidbody rb;
     // Start is called before the first frame update
@@ -15,15 +15,16 @@ public class JotaMarioController : MonoBehaviour
         MovePlayer();
     }
     void MovePlayer() {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * movementSpeed;
-        if(movementDirection != Vector3.zero) {
+        if(Input.GetButton("Vertical") || Input.GetButton("Horizontal")) {
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
             var vector2 = new Vector2(-horizontalInput, verticalInput);
             var angle = Vector2.SignedAngle(Vector2.up, vector2);
             Debug.Log(angle + " " + vector2);
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
-            transform.Translate(( ( transform.forward * verticalInput ) + ( transform.right * -horizontalInput ) ) * Time.deltaTime * movementSpeed);
+            var movement = ( transform.forward * verticalInput ) +
+                           ( transform.right * -horizontalInput );
+            transform.Translate(movement * Time.deltaTime * movementSpeed);
         }
     }
 }
