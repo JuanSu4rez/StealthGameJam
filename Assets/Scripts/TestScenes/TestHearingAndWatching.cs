a6660dd90120ee8a4e4df9e3c224da69a13f466b
Andres Vargas <archarry>
2022-06-13 08:23:04 -0500

    Controlling animations by Multiplier

---
+++ Assets/Scripts/TestScenes/TestHearingAndWatching.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHearingAndWatching : MonoBehaviour
+{
+    public GameObject Soldier;
+    public float AnimationMutiplier = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+       var _animator = Soldier.GetComponent<Animator>();
+        _animator.SetFloat("animationMutiplier", AnimationMutiplier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
