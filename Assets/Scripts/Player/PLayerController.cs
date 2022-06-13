fb2f7681997a73ad78edb071796da151fac872a3
Sebatian Suarez <jnssuarez@gmail.com>
2022-06-13 22:33:36 +1200

    Dungeon added, lights adjusted

---
+++ Assets/Scripts/Player/PLayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerController : MonoBehaviour{
+
+    public float movementSpeed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start() {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update() {
+        MovePlayer();
+    }
+    void MovePlayer() {
+        if(Input.GetButton("Vertical") || Input.GetButton("Horizontal")) {
+            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxis("Horizontal");
+            var vector2 = new Vector2(-horizontalInput, verticalInput);
+            var angle = Vector2.SignedAngle(Vector2.up, vector2);
+            Debug.Log(angle + " " + vector2);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+            var movement = ( transform.forward * verticalInput ) +
+                           ( transform.right * -horizontalInput );
+            transform.Translate(movement * Time.deltaTime * movementSpeed);
+        }
+    }
+}
